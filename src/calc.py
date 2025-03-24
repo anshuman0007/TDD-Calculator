24a69b83e165799182987a15c4d37975fd60b24e
Anshuman Mishra <anshumanmishra79@gmail.com>
2025-03-24 12:55:47 +0530

    Added additional logic for handling >1000, multiple-chars, multiple-delimiters

--- src/calc.py
+++ src/calc.py
@@ -12,12 +12,12 @@ class Calc:
             match = re.match(r"//(.+)\n(.*)", numbers)
             if match:
                 delimiter, numbers = match.groups()
-                delimiter = re.split(r"\[|\]", delimiter) if "[" in delimiter else [delimiter]
+                delimiter = re.findall(r"\[(.*?)\]", delimiter) or [delimiter]
                 delimiter = "|".join(map(re.escape, delimiter))
 
         num_list = re.split(delimiter, numbers)
 
-        int_list = [int(n) for n in num_list if n]
+        int_list = [int(n) for n in num_list if n and int(n) <= 1000]
         
         negatives = [n for n in int_list if n < 0]
         if negatives:
@@ -26,7 +26,7 @@ class Calc:
         return sum(int_list)
 
 if __name__ == "__main__":
-    user_input = input("Enter numbers (comma-separated or custom delimiter): ").encode().decode("unicode_escape").strip()
+    user_input = input("Enter comma-separated numbers): ").encode().decode("unicode_escape").strip()
     #\n treated as new line "Error when using '//;\\n1;2"
     try:
         result = Calc.add(user_input)
