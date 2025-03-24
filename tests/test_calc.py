8be897b64f15904fe2a4275e347dbc451ea77dea
Anshuman Mishra <anshumanmishra79@gmail.com>
2025-03-24 12:29:24 +0530

    Added advanced test cases for newline delimiter, custom delimiter, and negative numbers

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -13,3 +13,16 @@ def test_two_numbers():
 def test_multiple_numbers():
     assert Calc.add("1,2,3,4") == 10
 
+def test_new_line_delimiter():
+    assert Calc.add("1\n2,3") == 6
+
+def test_custom_delimiter():
+    assert Calc.add("//;\n1;2") == 3
+
+def test_negative_number():
+    with pytest.raises(ValueError, match="negative numbers not allowed: -3"):
+        Calc.add("1,-3")
+
+def test_multiple_negative_numbers():
+    with pytest.raises(ValueError, match="negative numbers not allowed: -3,-5"):
+        Calc.add("1,-3,2,-5")
